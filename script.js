afda9139ba394746e2e748098fe6c2def05b7bd4
yashtomar246 <yashtomar246@gmail.com>
2026-06-12 00:40:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,17 +6,24 @@ const quotes = [
     "Believe in yourself ghochii 😤✨",
     "You are my reason to smile 😘💖",
     "Never stop shining cutuu🌟",
+    "Someone Who thanks the universe for you ❤️",
 ];
 
 const quoteBtn = document.getElementById("quoteBtn");
 const quoteText = document.getElementById("quoteText");
 
+let currentQuote = 0;
+
 quoteBtn.addEventListener("click", () => {
 
-    const random =
-        Math.floor(Math.random() * quotes.length);
+    quoteText.innerText = quotes[currentQuote];
 
-    quoteText.innerText = quotes[random];
+    currentQuote++;
+
+    if(currentQuote >= quotes.length){
+        currentQuote = 0;
+    }
+
 });
 
 const addBtn = document.getElementById("addBtn");
@@ -38,4 +45,55 @@ document.querySelectorAll(".gallery-item img")
 
     });
 
+document.querySelectorAll('.quote-box').forEach(box => {
+    box.addEventListener('click', () => {
+        box.classList.toggle('active');
+    });
 });
+
+});
+document.querySelectorAll('.superpower').forEach(card => {
+
+    card.addEventListener('click', () => {
+
+        card.classList.add('active');
+
+        setTimeout(() => {
+            card.classList.remove('active');
+        }, 300);
+
+    });
+
+});
+
+const startDate = new Date("2023-06-12T00:00:00");
+
+function updateCounter(){
+
+    const now = new Date();
+
+    const diff = now - startDate;
+
+    const days = Math.floor(diff / (1000*60*60*24));
+
+    const hours = Math.floor(
+        (diff % (1000*60*60*24)) / (1000*60*60)
+    );
+
+    const minutes = Math.floor(
+        (diff % (1000*60*60)) / (1000*60)
+    );
+
+    const seconds = Math.floor(
+        (diff % (1000*60)) / 1000
+    );
+
+    document.getElementById("days").innerText = days;
+    document.getElementById("hours").innerText = hours;
+    document.getElementById("minutes").innerText = minutes;
+    document.getElementById("seconds").innerText = seconds;
+}
+
+updateCounter();
+
+setInterval(updateCounter,1000);
